c84baee266b77b63e30c35431780a8494bcfcf60
bernabe fernandez <bernabefernandezg05@gmail.com>
2023-05-10 00:50:33 -0600

    Subimos carpeta de conexion

---
+++ conection/conex.php
@@ -0,0 +1,19 @@
+<?php
+//funcion de conexion
+function conectar(){
+    $user = "root";
+    $pass = "";
+    $server = "localhost";
+    $bd = "ichejav2";
+
+    //se hace la conexion a la base de datos
+    $cone = mysqli_connect($server, $user, $pass, $bd);
+
+    //validamos si la conexion es exitosa
+    if(!$cone){
+        die("Error al conectarse");
+    }
+    
+    return $cone;
+}
+?>
